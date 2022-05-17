30f995246f4f4c1fea3e5c1cf6360f3fc667a8a1
divya1viswanath <divyarugmini798@gmail.com>
2022-05-17 20:53:29 +0530

    add product created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import Addproduct from './component/Addproduct';
 
 function App() {
   return (
     <div>
-      
+      <Addproduct/>
     </div>
 }
 
