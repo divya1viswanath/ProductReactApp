469b613582add24a4276628bfdda8e6f31120a0a
divya1viswanath <divyarugmini798@gmail.com>
2022-05-18 16:10:26 +0530

    read addpost

--- src/component/Addproduct.js
+++ src/component/Addproduct.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Addproduct = () => {
+    var [name,setProduct]=useState("")
+    var [Price,setPrice]=useState("")
+    var [Description,setDescription]=useState("")
+    var [Category,setCategory]=useState("")
+const subData=()=>{
+    const data={"name":name,"Price":Price,"Description":Description,"Category":Category}
+    console.log(data)
+}
   return (
     <div>
         <Header/>
@@ -9,28 +17,25 @@ const Addproduct = () => {
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-2 col-lg-12 col-xl-12 col-xxl-12">
                     <div className="row g-3">
-                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label for="" className="form-label"> id</label>
-                            <input type="text" className="form-control"/>
-                        </div>
+                    
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label for="" className="form-label">Product Title</label>
-                            <input type="text" className="form-control"/>
+                            <input onChange={(y)=>{setProduct(y.target.value)}} type="text" className="form-control"/>
                         </div>                                   
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label for="" className="form-label">Price</label>
-                            <input type="text" className="form-control"/>
+                            <input onChange={(y)=>{setPrice(y.target.value)}} type="text" className="form-control"/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label for="" className="form-label">Description</label>
-                            <input type="text" className="form-control"/>
+                            <input onChange={(y)=>{setDescription(y.target.value)}} type="text" className="form-control"/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label for="" className="form-label">Category</label>
-                            <input type="text" className="form-control"/>
+                            <input onChange={(y)=>{setCategory(y.target.value)}} type="text" className="form-control"/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <button className="btn btn-primary">SUBMIT</button>
+                            <button onClick={subData} className="btn btn-primary">SUBMIT</button>
                         </div>
                     </div>
                 </div>
